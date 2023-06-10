e9a1ab2f5d3557f4777b086193ceb0755999b553
Ahmed Qaddoura <hem-plo@hotmail.com>
2023-06-10 19:18:21 +0400

    Ep07 Functions

---
+++ 02 Basics/After 07 Functions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyProgram
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Title = "My first program";
+			string username = AskForInput("Hello, My name is Ahmed.\nWhat's your name?!");
+			BotMessage("Hello, " + username + "! Nice to meet you.");
+			string age = AskForInput("How old are you?");
+			BotMessage("I am 35 YO!");
+
+			//To prevent the program from exiting
+			Console.BackgroundColor = ConsoleColor.Yellow;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("\n\n\nPress any key to exit...");
+			Console.ReadKey();
+		}
+		static string AskForInput(string prompt)
+		{
+			BotMessage(prompt);
+			Console.BackgroundColor = ConsoleColor.Black;
+			Console.ForegroundColor = ConsoleColor.White;
+			return Console.ReadLine();
+		}
+		static void BotMessage(string message)
+		{
+			Console.BackgroundColor = ConsoleColor.White;
+			Console.ForegroundColor = ConsoleColor.Black;
+			Console.WriteLine(message);
+		}
+	}
+}
---
+++ 07 Functions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyProgram
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			PrintMessage();
+			int z = Multiply(50, 5);
+			Console.WriteLine("Z: " + z);
+			//To prevent the program from exiting
+			Console.BackgroundColor = ConsoleColor.Yellow;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("\n\nPress any key to exit...");
+			Console.ReadKey();
+		}
+
+		static void PrintMessage()
+		{
+			Console.WriteLine("Hello, I am a Method");
+		}
+
+		static int Multiply(int x, int y)
+		{
+			return x * y;
+		}
+	}
+}
